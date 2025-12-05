053968207b64a1a054ab53ebc58da9911c297c80
Mads Hartmann <mads379@gmail.com>
2025-12-05 20:11:00 +0100

    Add raycast-script-commands folder

---
+++ raycast-script-commands/ona-sync-zed-ssh-connections.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Ona: Sync Zed SSH Connections
+# @raycast.mode fullOutput
+#
+# Optional parameters:
+# @raycast.icon 🔧
+# @raycast.packageName Hartmann's Tools
+
+~/.opam/default/bin/ona-sync-zed-ssh-connections --once
